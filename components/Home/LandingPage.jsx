870253ae98267808d6e9edc21fee3203a04c9cd6
legcy <legcy143@gmail.com>
2023-05-27 03:40:38 +0530

    minor change

---
+++ components/Home/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React , { Fragment }from 'react'
+import style from "./styles/LandingPage.module.scss"
+import Homebg from './Homebg'
+
+const LandingPage = () => {
+  return (
+        <main className={style.heroSection}>
+            <h1>hii</h1>
+            <h1>hii</h1>
+        </main>
+  )
+}
+
+export default LandingPage
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import BackgroudGrad from '@/components/background/BackgroudGrad';
 import HomeScreen from '@/components/Home/HomeScreen';
+import LandingPage from '@/components/Home/LandingPage';
 
 export default function Home() {
   return (
     <>
     <HomeScreen />
+    {/* <LandingPage/> */}
     </>
   )
 }
